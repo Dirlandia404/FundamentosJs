4ee062512bcd18e97390772ede6e82adea9ad6dd
Dirlandia <dirlandiaoliveira@alu.ufc.br>
2021-10-13 22:14:18 -0300

    Dia 13

---
+++ Cursos/FundamentosJs/estruturaControle/doWhile.js
@@ -0,0 +1,11 @@
+function getInteiroRandom(min, max) {
+  const valor = Math.random() * (max - min) + min;
+  return Math.floor(valor);
+}
+let opcao;
+
+do {
+  opcao = getInteiroRandom(-1, 10);
+  console.log(`opcao foi ${opcao}`);
+} while (opcao != -1);
+console.log("acabou");
